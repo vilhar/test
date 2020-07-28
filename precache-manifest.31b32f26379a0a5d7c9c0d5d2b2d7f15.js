8f11c6845e09a3e6d67297959563b7ed6ea3d4f9
vilhar <ville.harju@leanware.fi>
2020-07-28 13:00:25 +0300

    Updates

---
+++ precache-manifest.31b32f26379a0a5d7c9c0d5d2b2d7f15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5df21f9053a5faa1ea8015ab580fdc0",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "3f1e48245770c63a9e22",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b3287c65a36681af3eb",
+    "url": "/test/static/js/2.fa9dc631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.fa9dc631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1e48245770c63a9e22",
+    "url": "/test/static/js/main.f5ecc38f.chunk.js"
+  },
+  {
+    "revision": "6940d8cfeca5c3392541",
+    "url": "/test/static/js/runtime-main.63f50a78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
